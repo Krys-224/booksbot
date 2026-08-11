cf1d67065d6ab90cbc7ac17533e54e5539c1c764
Krys-224 <kmiatselskaya@cubecom.ca>
2026-08-11 17:42:44 -0400

    Update omvic_dealers.py

--- books/spiders/omvic_dealers.py
+++ books/spiders/omvic_dealers.py
@@ -5,29 +5,35 @@ class OmvicDealersSpider(scrapy.Spider):
     name = "omvic_dealers"
     allowed_domains = ["omvic.ca"]
 
-    custom_settings = {
-        "ZYTE_API_TRANSPARENT_MODE": True,
-    }
-
     def start_requests(self):
-        url = "https://www.omvic.ca/dealer-search/?dealership_city=Toronto&search_type=dealership"
+        url = (
+            "https://www.omvic.ca/dealer-search/"
+            "?dealership_city=Toronto&search_type=dealership"
+        )
 
         yield scrapy.Request(
             url,
             callback=self.parse,
             meta={
-                "zyte_api": {
+                "zyte_api_automap": {
                     "browserHtml": True,
                 }
             },
         )
 
     def parse(self, response):
-        text = response.xpath("//body//text()[normalize-space()]").getall()
-        clean_text = [x.strip() for x in text if x.strip()]
+        text = response.xpath(
+            "//body//text()[normalize-space()]"
+        ).getall()
+
+        clean_text = [
+            x.strip()
+            for x in text
+            if x.strip()
+        ]
 
         yield {
             "page_title": response.css("title::text").get(),
             "url": response.url,
-            "sample_page_text": clean_text[-100:],
+            "sample_page_text": clean_text[-200:],
         }
