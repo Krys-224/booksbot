9ab43e472888b88cc30492a1f381e475d8f5bacd
Krys-224 <kmiatselskaya@cubecom.ca>
2026-08-11 17:27:30 -0400

    Update omvic_dealers.py
    Add OMVIC dealer test spider

--- books/spiders/omvic_dealers.py
+++ books/spiders/omvic_dealers.py
@@ -1,1 +1,21 @@
+import scrapy
 
+
+class OmvicDealersSpider(scrapy.Spider):
+    name = "omvic_dealers"
+    allowed_domains = ["omvic.ca"]
+
+    start_urls = [
+        "https://www.omvic.ca/dealer-search/?dealership_city=Toronto&search_type=dealership"
+    ]
+
+    def parse(self, response):
+        self.logger.info("OMVIC page loaded: %s", response.url)
+
+        yield {
+            "page_title": response.css("title::text").get(),
+            "url": response.url,
+            "dealer_names_found": response.xpath(
+                "//*[contains(@class,'dealer') or contains(@class,'result')]//text()"
+            ).getall()[:20],
+        }
